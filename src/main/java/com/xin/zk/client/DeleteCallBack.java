46629946fc83191e27ded859f8e7cb1d150c8882
zhangxingits <zxae868@gmail.com>
2018-09-30 15:13:35 +0800

    first

--- src/main/java/com/xin/zk/client/DeleteCallBack.java
+++ src/main/java/com/xin/zk/client/DeleteCallBack.java
@@ -4,7 +4,7 @@ import org.apache.zookeeper.AsyncCallback.VoidCallback;
 
 public class DeleteCallBack implements VoidCallback {
 	public void processResult(int rc, String path, Object ctx) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub haha
 		System.out.println("删除节点:"+path);
 		System.out.println((String)ctx);
 	}
